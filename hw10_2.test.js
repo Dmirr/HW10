c4097e250a79c878c3ca5316ecbe49c7a556076d
Dmirr <dmitriyrodionow@gmail.com>
2022-12-21 22:42:02 +0300

    12

--- hw10_2.test.js
+++ hw10_2.test.js
@@ -2,6 +2,6 @@ const topStudents = require("./hw10_2.js");
 
 describe("Test top student", () => {
   it("Проверка на больше или равно ", () => {
-    expect(topStudents(35)).toBeGreaterThanOrEqual(35);
+    expect(studentsTop).toBeGreaterThanOrEqual(35);
   });
 });
